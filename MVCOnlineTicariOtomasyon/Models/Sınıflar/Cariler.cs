ac2d8d96b38bcfd9f324189c818c031de0d705c3
burcu <burcumenguc06@gmail.com>
2025-02-25 16:10:37 +0300

    added departman page

--- MVCOnlineTicariOtomasyon/Models/Sınıflar/Cariler.cs
+++ MVCOnlineTicariOtomasyon/Models/Sınıflar/Cariler.cs
@@ -13,11 +13,12 @@ namespace MVCOnlineTicariOtomasyon.Models.Sınıflar
         public int CariId { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30,ErrorMessage ="En fazla 30 karakter yazabilirsiniz")]
         public string CariAd { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz")]
         public string CariSoyad { get; set; }
 
         [Column(TypeName = "Varchar")]
@@ -27,7 +28,7 @@ namespace MVCOnlineTicariOtomasyon.Models.Sınıflar
         [Column(TypeName = "Varchar")]
         [StringLength(50)]
         public string CariMail { get; set; }
-
+        public bool Durum { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
 
     }
